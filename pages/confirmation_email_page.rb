154c7d74600ea65fc360cf0ae0a69ff6fafe8946
Roman Parashchenko <romikoops1@gmail.com>
2016-07-01 00:24:07 +0200

    removed outdated monkey patch

--- pages/confirmation_email_page.rb
+++ pages/confirmation_email_page.rb
@@ -2,6 +2,4 @@ class ConfirmationEmailPage < Howitzer::Web::Page
   url '/users/confirmation?confirmation_token={token}'
   validate :url, /\/users\/confirmation/
 
-  #TODO remove it when new :open method without validation will be implemented
-  def self.given; end
 end
